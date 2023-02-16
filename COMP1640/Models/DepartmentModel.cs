2998bc6f5918fd160f64a8f086d8ee52a018a865
Bui Ngoc Huy <buingochuy124@gmail.com>
2023-02-16 16:34:23 +0700

    create department model

---
+++ COMP1640/Models/DepartmentModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace COMP1640.Models
+{
+    public class DepartmentModel
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string Id { get; set; }
+        public string Name { get; set; }
+
+        [ForeignKey("User")]
+        public string CordinatorId { get; set; }
+        public AppUserModel Cordinator { get; set; }
+    }
+}
