8dc33431dc2ac2ece984bc3eaecad94586f5336a
tuanle273 <tuanle273@gmail.com>
2023-03-15 14:51:22 +0700

    add statistic successfully

--- COMP1640/Areas/User/Controllers/DashboardController.cs
+++ COMP1640/Areas/User/Controllers/DashboardController.cs
@@ -38,19 +38,44 @@ namespace COMP1640.Areas.User.Controllers
         .Select(g => new { Department = g.Key, Count = g.Count() })
         .ToDictionary(x => x.Department, x => x.Count);
 
-
-
-
-
-
             int totalPostCount = _context.Posts.Count();
             int totalDepartmentCount = _context.Departments
             .Select(d => d.Name)
             .Distinct()
              .Count();
+            int totalUserWithoutDepartmentCount = _context.Users
+              .Where(u => u.Department == null)
+             .Count();
+            int totalIdeasWithoutCommentCount = _context.Posts
+              .Select(d => d.Content)
+            .Distinct()
+            .Count();
+            int totalAnonymousIdeaCount = _context.Posts
+             .Where(p => p.IsAnonymous)
+             .Count();
+
+            int totalAnonymousCommentCount = _context.PostComments
+               .Where(p => p.IsAnonymous)
+               .Count();
+            int totalCategoryCount = _context.Categories
+          .Select(d => d.Name)
+          .Distinct()
+           .Count();
+            int totalUserCount = _context.Users
+            .Select(d => d.FirstName)
+          .Distinct()
+           .Count();
+
+            ViewBag.TotalUserWithoutDepartmentCount = totalUserWithoutDepartmentCount;
             ViewBag.TotalPostCount = totalPostCount;
             ViewBag.TotalDepartmentCount = totalDepartmentCount;
+            ViewBag.TotalAnonymousIdeaCount = totalAnonymousIdeaCount;
+            ViewBag.TotalIdeasWithoutCommentCount = totalIdeasWithoutCommentCount;
+            ViewBag.TotalAnonymousCommentCount = totalAnonymousCommentCount;
+            ViewBag.TotalCategoryCount = totalCategoryCount;
+            ViewBag.TotalUserCount = totalUserCount;
             return View(postCounts);
+
         }
 
 
